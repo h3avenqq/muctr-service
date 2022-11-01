0be891a281c366ab2200516fef8c5fe483ee7a81
VVoron <vkunievskiy@list.ru>
2022-11-01 12:56:03 +0300

    Added FirstSiteVersion

---
+++ WebApp/MuctrSite/Controllers/DepartmentsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MuctrSite.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GetDepartment()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApp/MuctrSite/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MuctrSite.Models;
+using System.Diagnostics;
+
+namespace MuctrSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult GetAllNews()
+        {
+            return View();
+        }
+        public IActionResult GetAllActions()
+        {
+            return View();
+        }
+
+        public IActionResult GetNews()
+        {
+            return View();
+        }
+        public IActionResult GetActions()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
