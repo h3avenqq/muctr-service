40cb7e73d4e4c8569bbaba093d8be3d019a95a2f
Maxim Shamrev <shmarev082@gmail.com>
2022-12-21 13:03:49 +0300

    test json sending

---
+++ WebAPI/MuctrService.Backend/MuctrService.WebApi/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+
+namespace MuctrService.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class TestController : BaseController
+    {
+        [HttpGet]
+        public async Task<ActionResult> GetAll()
+        {
+            var json = System.IO.File.ReadAllText("Controllers/testJson.txt");
+
+            var objects = JsonArray.Parse(json);
+
+            return Ok(objects);
+        }
+    }
+}
